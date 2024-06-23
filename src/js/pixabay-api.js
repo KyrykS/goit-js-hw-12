4d6b54416834ccfd8e8915ed68a60ac447aac579
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-06-23 19:56:40 +0200

    up

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -7,17 +7,17 @@ import axios from 'axios';
 const API_KEY = '44464859-33f7988efa5c5083e6b56b801';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query, page = 1) {
-  const params = {
+export async function fetchImages(query, page = 1, perPage = 15) {
+const response = await axios.get(BASE_URL, {
+  params : {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     page,
-    per_page: 12,
-  };
-
-  const response = await axios.get(BASE_URL, { params });
-  return response.data;
+    per_page: perPage,
+  },
+  });
+ return response.data;
 }
